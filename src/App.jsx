94a55bab7c35337c2be78ad53abca9564b1005d1
von-ai <kuhhakuzuhdi@gmail.com>
2024-11-03 09:14:43 +0800

    all-features

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import DigitalClock from './DigitalClock';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <DigitalClock />
+      {/* <img src="/images/frieren.jpeg" alt="frieren" className="frieren" /> */}
+    </>
+  );
+}
+
+export default App;
